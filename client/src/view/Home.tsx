72d297d057d8d75500f73032596ffd3b4a882f97
Dmitriy Grosh <dmitriy.grosh@gmail.com>
2022-02-24 18:12:08 +0300

    delete unusable code

--- client/src/view/Home.tsx
+++ client/src/view/Home.tsx
@@ -3,19 +3,7 @@ import api from '../shared/api';
 
 const Home = () => {
   const [count, setCount] = useState<Array<number>>([1]);
-  const divRef = useRef<HTMLDivElement>(null);
-  console.log('==========>divRef', divRef);
 
-  useEffect(() => {
-    if (divRef.current !== null) {
-      divRef.current.addEventListener('scroll', () => console.log('==========>1', 1));
-    }
-    console.log('==========>divRef effext', divRef);
-  }, []);
-
-  useEffect(() => {
-    console.log('==========>2', 2);
-  }, []);
   const handleGetUsers = async () => {
     try {
       const users = await api.get('/users');
@@ -28,8 +16,9 @@ const Home = () => {
   const handleCount = () => {
     setCount((prev) => [...prev, prev[prev.length - 1] + 1]);
   };
+
   return (
-    <div ref={divRef}>
+    <div>
       {count.map((el) => (
         <span key={el}>{el}</span>
       ))}
